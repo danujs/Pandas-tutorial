3744de42c269b813b7aba3a1461fec88b15d30e6
danujs <86118475+danujs@users.noreply.github.com>
2023-05-21 14:55:21 +0700

    Update and rename mergeFile.ipnyb to mergeFile.py
    Try this python code using jupyter lab.

--- mergeFile.py
+++ mergeFile.py
@@ -1,6 +1,6 @@
-**Merge two Data Frames
+# **Merge two Data Frames
+# >Arranging the rows and columns
 
->Arranging the rows and columns
 n_rows = 5
 n_cols = 5
 cols = tuple('ABCDE')
@@ -9,23 +9,26 @@ df = pd.DataFrame(np.random.randint(1, 20, size=(n_rows, n_cols)),
                  columns=cols)
 df
 
->Eliminate the rows of index 1 and 4 using .drop()
->Store the data to variable df1
+# >Eliminate the rows of index 1 and 4 using .drop()
+# >Store the data to variable df1
+
 df1 = df.copy(deep=True)
 df1 = df1.drop([1, 4])
 df1
 
->Eliminate the rows of index 0 and 3 using .drop()
->Store the data to variable df2
+# >Eliminate the rows of index 0 and 3 using .drop()
+# >Store the data to variable df2
+
 df2 =df.copy(deep=True)
 df2 =df2.drop([0, 3])
 df2
 
->Join the intersection data between df1 and df2 using 'inner'
+# >Join the intersection data between df1 and df2 using 'inner'
 df_inner = pd.merge(df1, df2, how='inner')
 df_inner
 
->Join all the data between df1 and df2 using 'outer'
+# >Join all the data between df1 and df2 using 'outer'
 df_outer = pd.merge(df1, df2, how='outer') #barisnya akan digabung, dimana baris adopsinya hanya diambil 1 saja
 df_outer
 
+# all the data have been merged. Completed.
